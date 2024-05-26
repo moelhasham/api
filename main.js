ebe822886d7bb20ec4a5cd69dd1ac83a9910be67
mohammed <moelhasham@uotlook.com>
2024-05-26 21:12:01 +0400

    add cors

--- main.js
+++ main.js
@@ -8,13 +8,16 @@ const app = express()
 app.use(express.json())
 const axios = require('axios');
 
+const corsOptions = {
+  origin: 'http://localhost:5174',
+ 
+}
 
-
-app.get("/" , async (req,res) => {
+app.get("/" ,cors(corsOptions), async (req,res) => {
   res.json("hello mohammed")
 })
 
-app.get("/prof" , async (req,res) => {
+app.get("/prof" ,cors(corsOptions), async (req,res) => {
   res.json("https://media.wired.com/photos/598e35994ab8482c0d6946e0/master/w_1920,c_limit/phonepicutres-TA.jpg")
 })
 
@@ -22,6 +25,6 @@ app.get("/prof" , async (req,res) => {
 const port = process.env.PORT || 8002
 
 app.listen(process.env.PORT || 8002, (res,req) => {
-    console.log(`app run on `)
+    console.log(`app run on`)
 })
 
