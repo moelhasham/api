08fc5e2e4147ec2f138dda947d14a8e2b2c603de
mohammed <moelhasham@uotlook.com>
2024-05-28 02:24:39 +0400

    h

--- main.js
+++ main.js
@@ -8,7 +8,7 @@ app.use(express.json())
 app.use(function (req, res, next) {
 
   // Website you wish to allow to connect
-  res.setHeader('Access-Control-Allow-Origin', 'https://dapper-bombolone-84d12f.netlify.app');
+  res.setHeader('Access-Control-Allow-Origin', '*');
 
   // Request methods you wish to allow
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
