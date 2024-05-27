9a964c14169d0a9be325d9c62aed638250cb538d
mohammed <moelhasham@uotlook.com>
2024-05-28 02:18:37 +0400

    fun

--- main.js
+++ main.js
@@ -5,7 +5,22 @@ const app = express()
 app.use(express.json())
 
 
+app.use(function (req, res, next) {
 
+  // Website you wish to allow to connect
+  res.setHeader('Access-Control-Allow-Origin', 'https://dapper-bombolone-84d12f.netlify.app');
+
+  // Request methods you wish to allow
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+  // Request headers you wish to allow
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+  res.setHeader('Access-Control-Allow-Credentials', true);
+
+  
+  next();
+});
 
 app.use(cors({
   origin: "*",
