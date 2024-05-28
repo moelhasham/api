91b28cd7b09c2e7863de43c61af917dd86ccf9ee
mohammed <moelhasham@uotlook.com>
2024-05-28 14:19:20 +0400

    l

--- main.js
+++ main.js
@@ -4,7 +4,11 @@ var cors = require('cors')
 const app = express()
 app.use(express.json())
 
-
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 
 app.use(cors({
