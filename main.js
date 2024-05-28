6020db74c62841013335330b47bf8eaf3d23fddb
mohammed <moelhasham@uotlook.com>
2024-05-29 02:13:27 +0400

    n

--- main.js
+++ main.js
@@ -3,13 +3,14 @@ require('dotenv').config()
 var cors = require('cors')
 const {ver} = require("./util/vervy")
 const app = express()
+const axios = require("axios")
 app.use(express.json())
 
 
 var corsOptions = {
   origin: 'https://sirily.netlify.app'
 };
-app.use(cors(corsOptions));
+app.use(cors());
 
 app.get("/" , async (req,res) => {
   await res.json("server run")
@@ -20,52 +21,43 @@ app.get("/user" , async (req,res) => {
 })
 
 app.post("/pay",  async (req,res) => {
-  
-  const Msisdn = req.body.Msisdn
-  const BirthYear = req.body.BirthYear 
-  const InvoiceNo =  req.body.InvoiceNo
+  const {Msisdn,BirthYear,InvoiceNo} = req.body
   const Amount = 1 
   const Category = 11
-
-  const resp = await fetch("https://pgw.almadar.ly/api/Validate", {
-    method:'POST',
-    body: JSON.stringify({Msisdn,BirthYear,InvoiceNo,Amount,Category}),
-    headers:{
-      "Content-Type": "application/json",
-      "SharedKey": "u-srNIFp8Ch2IwDFfh5BT6LoQ4EvwxI5",
-      "Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJodHRwOi8vc2NoZW1hcy54bWxzb2FwLm9yZy93cy8yMDA1LzA1L2lkZW50aXR5L2NsYWltcy9uYW1lIjoiU2FkYWRQR1ciLCJjb3Jwb3JhdGVTZXJ2aWNlSWQiOjIzOTksImFwcElkIjoiMTQxZTNiMDY4MDQ5NDk0Y2E5Mjc4YjY2ZmUzMGU2OWYiLCJleHAiOjIwMzAyNjczNjUsImlzcyI6IkNvcmV0ZWMiLCJhdWQiOiJTQURBRFBHVyJ9.PSvwNOLznVSVFg5uJofg812gcj9zfAZnkCMw7gCfgqw",
-      "Access-Control-Allow-Origin": "*",
-    }
+  const url = "https://pgw.almadar.ly/api/Validate"
+  const headers = { Authorization: "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJodHRwOi8vc2NoZW1hcy54bWxzb2FwLm9yZy93cy8yMDA1LzA1L2lkZW50aXR5L2NsYWltcy9uYW1lIjoiU2FkYWRQR1ciLCJjb3Jwb3JhdGVTZXJ2aWNlSWQiOjIzOTksImFwcElkIjoiMTQxZTNiMDY4MDQ5NDk0Y2E5Mjc4YjY2ZmUzMGU2OWYiLCJleHAiOjIwMzAyNjczNjUsImlzcyI6IkNvcmV0ZWMiLCJhdWQiOiJTQURBRFBHVyJ9.PSvwNOLznVSVFg5uJofg812gcj9zfAZnkCMw7gCfgqw"}
+  let d = []
+  await axios.post(url, {Msisdn,BirthYear,InvoiceNo,Amount,Category} ,{headers})
+   .then( data => {
+     const a = data.data
+     d.push(a)
+    
    })
-   
-    const da = await resp.json()
-  
-    res.json(da)
-
-   
+    
+   res.json(d)
   
 
 })
 
-app.post("/confirm", async (req,res) => {
+// app.post("/confirm", async (req,res) => {
 
-  const  TransactionId = req.body.TransactionId
-  const  OTP = req.body.OTP
-  // const  amount = req.body.amount
-  // const  invoice_no = req.body.invoice_no
-  const data = await fetch("https://pgw.almadar.ly/api/Pay", {
-    method:'POST',
-    body: JSON.stringify({TransactionId,OTP}),
-    headers:{
-      "Content-Type": "application/json",
-      "SharedKey": "u-srNIFp8Ch2IwDFfh5BT6LoQ4EvwxI5",
-      "Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJodHRwOi8vc2NoZW1hcy54bWxzb2FwLm9yZy93cy8yMDA1LzA1L2lkZW50aXR5L2NsYWltcy9uYW1lIjoiU2FkYWRQR1ciLCJjb3Jwb3JhdGVTZXJ2aWNlSWQiOjIzOTksImFwcElkIjoiMTQxZTNiMDY4MDQ5NDk0Y2E5Mjc4YjY2ZmUzMGU2OWYiLCJleHAiOjIwMzAyNjczNjUsImlzcyI6IkNvcmV0ZWMiLCJhdWQiOiJTQURBRFBHVyJ9.PSvwNOLznVSVFg5uJofg812gcj9zfAZnkCMw7gCfgqw",
-      "Access-Control-Allow-Origin": "*",
-    }
-   })
-   const result = await data.json()
-   res.json(result)
-})
+//   const  TransactionId = req.body.TransactionId
+//   const  OTP = req.body.OTP
+//   // const  amount = req.body.amount
+//   // const  invoice_no = req.body.invoice_no
+//   const data = await fetch("https://pgw.almadar.ly/api/Pay", {
+//     method:'POST',
+//     body: JSON.stringify({TransactionId,OTP}),
+//     headers:{
+//       "Content-Type": "application/json",
+//       "SharedKey": "u-srNIFp8Ch2IwDFfh5BT6LoQ4EvwxI5",
+//       "Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJodHRwOi8vc2NoZW1hcy54bWxzb2FwLm9yZy93cy8yMDA1LzA1L2lkZW50aXR5L2NsYWltcy9uYW1lIjoiU2FkYWRQR1ciLCJjb3Jwb3JhdGVTZXJ2aWNlSWQiOjIzOTksImFwcElkIjoiMTQxZTNiMDY4MDQ5NDk0Y2E5Mjc4YjY2ZmUzMGU2OWYiLCJleHAiOjIwMzAyNjczNjUsImlzcyI6IkNvcmV0ZWMiLCJhdWQiOiJTQURBRFBHVyJ9.PSvwNOLznVSVFg5uJofg812gcj9zfAZnkCMw7gCfgqw",
+//       "Access-Control-Allow-Origin": "*",
+//     }
+//    })
+//    const result = await data.json()
+//    res.json(result)
+// })
 
 // app.get("/prof" ,cors(corsOptions), async (req,res) => {
 //   res.json("https://media.wired.com/photos/598e35994ab8482c0d6946e0/master/w_1920,c_limit/phonepicutres-TA.jpg")
